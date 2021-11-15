963a31a2c0476accb18bd55c492779d835c4599a
Your Name <you@example.com>
2021-11-15 23:39:51 +0000

    pythintro

---
+++ file1.py
@@ -0,0 +1,120 @@
+# # print(10*2
+# # )
+
+# # print(10/2)
+
+# # print(10//4)
+# # print(10/4)
+# # print(10%4)
+
+# # print(10 * (5 + 2))
+
+# # numone = int(input("Please enter a number"))
+# # numtwo = int(input("Please enter a number"))
+# # print(numone + numtwo)
+
+# # print("Hello World")
+
+
+# # firstname = input("Please enter First Name")
+# # lastname = input("Please enter Last Name")
+# # print("Hello" +" "+ firstname +" "+ lastname)
+
+
+# number1 = input("enter whole number:")
+# number2 = input("enter decimal number:")
+
+# integernum = int(number1)
+# floatnum = float(number2)
+# roundnum = int(round(floatnum))
+# print(number1)
+# print(number2)
+# print(roundnum)
+
+# textvar1 = "ilikepies"
+
+# textvar2 = textvar1.isalpha() 
+# print(textvar2)
+
+
+# pet
+# name = "Dog"
+# age = 3
+# bark = True
+# tweet=  False
+
+
+# print("My pet it called", name, ", He is", age, "years old.")
+# print("Statement:", name, "barks.", bark)
+# print("Statement:", name, "tweets.", tweet)
+
+
+# shoppinglist= ["Meat", "Veggies", "Cake", "Beer for the weekend."]
+
+# print(shoppinglist)
+
+# shoppinglist.append("Parsnips")
+
+# print(shoppinglist)
+
+# coolcows=["Winnie the Moo", "Milkshake", "Mooana"]
+# coolsheep=["Baaaart", "Baaarnaby"]
+# coolpigs=["Chris p. Bacon", "Hamlet", "Hogwarts"]
+
+# coolanimals = [coolcows, coolpigs, coolsheep]
+
+# print(coolanimals[0][2])
+
+# books={"Book 1 yo":"That guy", "The Book":"Other guy", "Nah":"John Yah"}
+
+# print(books["Nah"])
+# print(books["Book 1 yo"])
+# print(books["The Book"])
+
+# var1 = "ilikepies"
+# # var1 = 3
+# var2 = 6
+
+# if var1 < var2:
+#     print(var1, "is less than", var2)
+# else:
+#         print(var1, "is not less than ", var2)
+
+# if var1 >= var2:
+#     print(var1, "is greater than or equal to", var2)
+# else:
+#         print(var1, "is not greater than ", var2)        
+
+# print("this is the rest of the program")
+
+# if var1.isdigit() or not (var2 > 3 amd var2 < 10):
+#     print("the answer was True")
+# else:
+#     print("the answer was False")
+
+
+# Mark = int(input("Please enter mark:"))
+# Distinction = 85
+# Pass = 65
+# Fail = 64
+# if Mark >= Distinction:
+#     print("Distinction")
+# elif Mark >= Pass:
+#     print("Pass")
+# else:
+#     print("Fail")
+
+# if Mark >= Distinction:
+#     print("Distinction")
+
+# if Mark >= Pass:
+#         print("Pass")
+
+# if Mark <= Fail:
+#     print("fail")
+
+integer = int(input("Enter number:"))
+if(integer%2==0):
+    print("Even")
+else:
+    print("Odd")
